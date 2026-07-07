4ad0702e63970204e42e7768d918454b34a0bba8
Vasavi <jellavasavi00@example.com>
2026-07-07 16:27:09 +0530

    msg

---
+++ 13-tfvars/dev/backend.tf
@@ -0,0 +1,7 @@
+
+    bucket       = "terraform-demo01-bucket-dev"
+    key          = "tfvars.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  
---
+++ 13-tfvars/prod/backend.tf
@@ -0,0 +1,7 @@
+
+    bucket       = "terraform-demo01-bucket-prod"
+    key          = "tfvars.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  # Enables native S3 state locking (Terraform 1.10+)
